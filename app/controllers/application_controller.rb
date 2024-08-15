0f6cd785f7e76191714ef38dd5a8840e55b93093
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-08-14 22:42:54 -0300

    Adding footer, navbar, implement stimulus components and messages errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  def render_turbo_flash
+    turbo_stream.update("flash", partial: "shared/flashes")
+  end
 end
